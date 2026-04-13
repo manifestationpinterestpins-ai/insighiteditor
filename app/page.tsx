7a957ee5853877d804ece5c52f0834d44ddc3d80
manifestationpinterestpins-ai <manifestationpinterestpins@gmail.com>
2026-04-13 23:13:19 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -51,10 +51,7 @@ const SendIcon = () => (
 )
 
 const RepostIcon = () => (
-  <svg width="22" height="22" viewBox="0 0 24 24" fill="white">
-    <path d="M19.998 9.497a1 1 0 0 0-1 1v4.228a3.274 3.274 0 0 1-3.27 3.27h-5.313l1.791-1.787a1 1 0 0 0-1.412-1.416L7.29 18.287a1 1 0 0 0-.294.707v.001c0 .023.012.042.013.065a.92.92 0 0 0 .281.643l3.502 3.504a1 1 0 0 0 1.414-1.414l-1.797-1.798h5.318a5.276 5.276 0 0 0 5.27-5.27v-4.228a1 1 0 0 0-1-1Zm-6.41-3.496-1.795 1.795a1 1 0 1 0 1.414 1.414l3.5-3.5a1.003 1.003 0 0 0 0-1.417l-3.5-3.5a1 1 0 0 0-1.414 1.414l1.794 1.794H8.27A5.277 5.277 0 0 0 3 9.271V13.5a1 1 0 0 0 2 0V9.271a3.275 3.275 0 0 1 3.271-3.27Z"/>
-  </svg>
-)
+  <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round">
 
 const BookmarkIcon = () => (
   <svg width="18" height="18" viewBox="0 0 24 24" fill="none">
