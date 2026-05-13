12a100e8de86e2a9e3c61f05037d84db29da9a6e
manifestationpinterestpins-ai <manifestationpinterestpins@gmail.com>
2026-05-13 19:27:25 +0530

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js"
 
-const SUPABASE_URL = "https://tcvbgaxdecarulgtnaml.supabase.co"
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRjdmJnYXhkZWNhcnVsZ3RuYW1sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY0OTYyMDMsImV4cCI6MjA5MjA3MjIwM30.qvccJtW3axBFYUZbHXS9-xDA_yMobF7NYYtkrHeH_M4"
+const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
